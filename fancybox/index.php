1d25a37eda5fa51869fddc7df757f8c972184c6e
bhanu007 <bhanu.gill@osscube.com>
2013-05-16 08:56:31 -0700

    fancy box is added(16/5/2013)

---
+++ fancybox/index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<script src="jquery-lib/jquery-1.8.3.js" type="text/javascript"></script>
+    <script src="./js/JScript.js" type="text/javascript"></script>
+    <link href="./css/reset.css" rel="stylesheet" />
+</head>
+<body>
+<div class="fancybox">
+	<div class="box">
+	<form action="1.php" method="get">
+
+		USERNAME<input type ="text" name="name"><br>
+		PASSWORD<input type ="text" name="pwd"><br>
+		<input type="submit">
+		<a href="#" id="close"><img	src="close.png"></a>
+	
+	</form>
+	</div>
+</div>
+
+<p> Click to display fancy box </p>
+
+<input type="button" class="b1" value="OpencustomFancyBox">
+</body>
+    
+</html>
---
+++ fancybox/js/JScript.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function(){
+	
+	$('.fancybox').css({"display":"none"});
+	$('.b1').click(function(){
+
+		$('.fancybox').css({"display":"block"});
+
+		$('.fancybox').css({"z-index":"999"});
+		
+	});
+	
+	
+	$('#close').click(function(){
+
+		$('.fancybox').css({"display":"none"});
+
+	
+		
+	});
+});
