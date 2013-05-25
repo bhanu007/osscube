a343860fda455d89c18b942ce2b9f6c0d3056c41
bhanu007 <bhanu.gill@osscube.com>
2013-05-25 00:53:54 -0700

    sliding jquery is added

---
+++ sliding_door/index.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="maincss.css">
+<div id="header">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li id="current"><a href="#">News</a></li>
+    <li><a href="#">Products</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</div>
